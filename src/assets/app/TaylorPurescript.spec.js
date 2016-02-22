7cafd14033ef86ec1bc1e3407639a0622bd6cdf0
Rahul Somasunderam <rsom@certifydatasystems.com>
2016-02-22 11:07:18 -0800

    Add purescript

---
+++ src/assets/app/TaylorPurescript.spec.js
@@ -0,0 +1,24 @@
+describe('Taylor Purescript Spec', function () {
+  return it('calls, values and instances match', function () {
+    var taylor = PS.TaylorPurescript; // <1>
+    var PI = 3.14159265359;
+
+    expect(taylor.pow(2)(-2)).toBe(0.25); // <2>
+    expect(taylor.pow(2)(-1)).toBe(0.5);
+    expect(taylor.pow(2)(0)).toBe(1);
+    expect(taylor.pow(2)(1)).toBe(2);
+    expect(taylor.pow(2)(2)).toBe(4);
+
+    expect(taylor.fact(0)).toBe(1);
+    expect(taylor.fact(1)).toBe(1);
+    expect(taylor.fact(2)).toBe(2);
+    expect(taylor.fact(3)).toBe(6);
+
+    expect(taylor.sine(0)).toBeCloseTo(0.0, 3);
+    expect(taylor.sine(PI / 4)).toBeCloseTo(0.707, 3);
+    expect(taylor.sine(PI / 3)).toBeCloseTo(0.866, 3);
+    expect(taylor.sine(PI / 2)).toBeCloseTo(1.0, 3);
+    expect(taylor.sine(PI)).toBeCloseTo(0.0, 3);
+  });
+});
+
